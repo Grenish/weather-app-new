1924ebe11d6c90902206887f5b441abe27abac94
Grenish rai <ariestheracer@gmail.com>
2023-10-02 15:48:55 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
 import React from "react";
-import UilReact from '@iconscout/react-unicons/icons/uil-react'
-import { TopButtons, Inputs, TimeAndLocation } from "./components";
+import { TopButtons, Inputs, TimeAndLocation, TempInfo, Forecast } from "./components";
+import getFormattedWeatherData from "./services/weatherService";
 
 const App = () => {
+
+  const feathWeather = async () => {
+    const data = await getFormattedWeatherData( {q: "London"});
+    console.log(data)
+  }
+
+  feathWeather()
+
   return (
     <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 shadow-xl shadow-gray-500">
       <TopButtons />
       <Inputs />
       
       <TimeAndLocation />
+      <TempInfo />
+      <Forecast title="horly forecast" />
+      <Forecast title="daily forecast" />
     </div>
   );
 };
