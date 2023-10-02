7d51865bfc108f511c1223072ba0e3356155897b
Grenish rai <ariestheracer@gmail.com>
2023-10-02 15:49:04 +0530

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,7 @@
+import Forecast from "./Forecast";
 import Inputs from "./Inputs";
+import TempInfo from "./TempInfo";
 import TimeAndLocation from "./TimeAndLocation";
 import TopButtons from "./TopButtons";
 
-export { TopButtons, Inputs, TimeAndLocation };
+export { TopButtons, Inputs, TimeAndLocation, TempInfo, Forecast };
